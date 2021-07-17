9dc5aa8520c536184c3fd71b28bb254079a7714a
Jason Hubbard <jasonhubb@gmail.com>
2021-07-17 16:47:36 -0700

    sets Head tags

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import { Fragment } from "react";
+import Head from "next/head";
 import { MongoClient } from "mongodb";
 
 import MeetupList from "../components/meetups/MeetupList";
 
 const HomePage = (props) => {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta name="description" content="Browse a huge list of highly active React meetups!"/>
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
 };
 
 export const getStaticProps = async () => {
@@ -17,7 +27,7 @@ export const getStaticProps = async () => {
 
   const meetups = await meetupsCollection.find().toArray();
   client.close();
-  
+
   return {
     props: {
       meetups: meetups.map((meetup) => ({
