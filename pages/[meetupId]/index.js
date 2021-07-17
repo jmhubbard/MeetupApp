22b1bfbcb4d46132cbf8dcd5da1657cecc410efa
Jason Hubbard <jasonhubb@gmail.com>
2021-07-16 21:13:25 -0700

    adds getStaticPaths and getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,38 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      { params: {
+        meetupId: "m1",
+      },},
+      { params: {
+        meetupId: "m2",
+      },},
+    ]
+  }
+};
+
+export const getStaticProps = async (context) => {
+  //fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Steam_Rising_from_Chicago_River.jpg/450px-Steam_Rising_from_Chicago_River.jpg",
+        id: meetupId,
+        title: "First meetup",
+        address: "Some Street 5, Some City",
+        description: "This is the first meetup spot",
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
