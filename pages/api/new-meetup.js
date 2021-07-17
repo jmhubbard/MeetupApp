4fc3a5538c4aac731b2778cd2c228b6f8e1e2df3
Jason Hubbard <jasonhubb@gmail.com>
2021-07-16 21:52:51 -0700

    creates api route new-meetup that sends data to db

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+//POST
+
+const handler = async (req, res) => {
+    if(req.method === "POST") {
+        const data = req.body;
+        // const {title, image, address, description} = data;
+        const apiRoute = process.env.API_ROUTE
+
+        const client = await MongoClient.connect(apiRoute);
+        const db = client.db();
+
+        const meetupsCollection = db.collection("meetups");
+        const result = await meetupsCollection.insertOne({data});
+
+        console.log(result);
+        client.close();
+
+        res.status(201).json({message: "Meetup inserted"});
+    }
+}
+
+export default handler;
