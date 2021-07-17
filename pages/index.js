8b79489f4a2ff7b2ef2f100afe33754b9fd3f8a1
Jason Hubbard <jasonhubb@gmail.com>
2021-07-16 21:52:05 -0700

    removes extra space in DUMMY_DATA

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const DUMMY_MEETUPS = [
     title: "A First Meetup",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Steam_Rising_from_Chicago_River.jpg/450px-Steam_Rising_from_Chicago_River.jpg",
-    address: " Some address 5, 12345 Some City",
+    address: "Some address 5, 12345 Some City",
     description: "This is our first meetup",
   },
   {
@@ -14,7 +14,7 @@ const DUMMY_MEETUPS = [
     title: "A Second Meetup",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Goose_Island_Brewery.jpg/800px-Goose_Island_Brewery.jpg",
-    address: " Some address 6, 12345 Some City",
+    address: "Some address 6, 12345 Some City",
     description: "This is our second meetup",
   },
 ];
